9f9cdacb1e3dc39ca7af0286037683de92719d1f
박기현 <50714602+kiryanchi@users.noreply.github.com>
2023-02-22 23:14:42 +0900

    Init(0.1): First Commit

---
+++ src/main/java/com/gdsc/canigraduate/CanigraduateApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.canigraduate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanigraduateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanigraduateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdsc/canigraduate/CanigraduateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.canigraduate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanigraduateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
