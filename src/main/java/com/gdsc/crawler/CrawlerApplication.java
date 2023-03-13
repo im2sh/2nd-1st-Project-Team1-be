4f6c89f6fd5522eb8d1406e9588f55dc29237113
kiryanchi <kiryanchi@naver.com>
2023-03-13 16:57:53 +0900

    Crawler 완성

---
+++ src/main/java/com/gdsc/crawler/CrawlerApplication.java
@@ -0,0 +1,26 @@
+package com.gdsc.crawler;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.regex.Pattern;
+
+public class CrawlerApplication {
+
+    static String regex = "^(19|20)\\d{2}$";
+
+    public static void main(String[] args) {
+        if (args.length != 1 || !Pattern.matches(regex, args[0])) {
+            System.out.println("올바른 연도를 입력해주세요.");
+            System.exit(1);
+        }
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions opts = new ChromeOptions();
+        opts.addArguments("--remote-allow-origins=*");
+
+        Crawler crawler = new Crawler(opts, 5);
+        crawler.start(args[0]);
+    }
+}
