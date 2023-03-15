6d8dcfdcba578097489750b143912a717d7ea9ba
kiryanchi <kiryanchi@naver.com>
2023-03-15 16:51:38 +0900

    refactoring(0.1): UserLecture 리팩토링

--- src/main/java/com/gdsc/canigraduate/domain/user/lecture/UserLecture.java
+++ src/main/java/com/gdsc/canigraduate/domain/user/lecture/UserLecture.java
@@ -1,8 +1,7 @@
-package com.gdsc.canigraduate.domain.lecture;
+package com.gdsc.canigraduate.domain.user.lecture;
 
 import com.gdsc.canigraduate.domain.BaseEntity;
 import com.gdsc.canigraduate.domain.user.User;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,13 +15,13 @@ import java.util.List;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class Lecture extends BaseEntity {
+public class UserLecture extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "LECTURE_ID")
     private Long id;
 
-    @ManyToOne(fetch=FetchType.LAZY,cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "USER_ID")
     private User user;
 
@@ -33,12 +32,12 @@ public class Lecture extends BaseEntity {
     private Integer cultureCredit;
     private Integer normalCredit;
 
-    @OneToMany(mappedBy = "lecture",cascade = CascadeType.ALL)
-    private List<LectureDetail> lectureDetails = new ArrayList<>();
+    @OneToMany(mappedBy = "userLecture", cascade = CascadeType.ALL)
+    private List<UserLectureDetail> lectureDetails = new ArrayList<>();
 
 
     @Builder
-    public Lecture(String semester, Integer majorCredit, Integer cultureCredit, Integer normalCredit){
+    public UserLecture(String semester, Integer majorCredit, Integer cultureCredit, Integer normalCredit) {
         this.semester = semester;
         this.majorCredit = majorCredit;
         this.cultureCredit = cultureCredit;
--- src/main/java/com/gdsc/canigraduate/repository/LectureRepository.java
+++ src/main/java/com/gdsc/canigraduate/repository/LectureRepository.java
@@ -1,10 +1,9 @@
 package com.gdsc.canigraduate.repository;
 
-import com.gdsc.canigraduate.domain.lecture.Lecture;
-import com.gdsc.canigraduate.domain.user.User;
+import com.gdsc.canigraduate.domain.user.lecture.UserLecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LectureRepository extends JpaRepository<Lecture, Long> {
+public interface LectureRepository extends JpaRepository<UserLecture, Long> {
 }
