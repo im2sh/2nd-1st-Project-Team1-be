715d6ca8648c564ca179135486855c7903f6cd1e
kiryanchi <kiryanchi@naver.com>
2023-03-29 15:53:42 +0900

    Swagger Config

---
+++ src/main/java/com/gdsc/canigraduate/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.gdsc.canigraduate.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(
+            @Value("${springdoc.title}") String title,
+            @Value("${springdoc.version}") String version,
+            @Value("${springdoc.description}") String description) {
+        Info info = new Info()
+                .title(title)
+                .version(version)
+                .description(description);
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
